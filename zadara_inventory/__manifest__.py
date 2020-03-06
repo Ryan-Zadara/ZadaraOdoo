beb1a45b708f044d3b7e9e8c127af8a74f74ea14
Ryan-Zadara <ryan@zadarastorage.com>
2020-03-06 19:48:35 +0000

    Zadara Inventory

--- zadara_inventory/__manifest__.py
+++ zadara_inventory/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "zadara_inventory",
+    'name': "Zadara Inventory",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
